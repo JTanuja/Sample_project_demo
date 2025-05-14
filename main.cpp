61c7b924ce4ac9e65122a8fdaae7251eee1d74c4
Tanuja <128579852+JTanuja@users.noreply.github.com>
2025-05-14 11:34:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main() 
 {
-    double num1 = 10 , num2 20;
-    cout<< calculate(num1, num2);
+    double num1 = 10 , num2= 20;
+    cout<< "Sum of 10 and 20 is "<<calculate(num1, num2)<<"\n";
 }
